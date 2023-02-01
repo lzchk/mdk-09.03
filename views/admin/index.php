bddf5d679fb7488de7ae22670f1158c8cb5aff5f
lzchk <crottop@mail.ru>
2023-02-01 11:25:02 +0300

    changte

--- views/admin/index.php
+++ views/admin/index.php
@@ -10,7 +10,7 @@ use yii\grid\GridView;
 /** @var app\models\UserSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Административная панель';
+$this->title = 'Административная Панель';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-b-index">
